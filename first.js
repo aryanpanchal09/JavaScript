f67abbed6d9ab271ee792f59e35daba1501a7144
Aryan <Aryantech03@gmail.com>
2024-10-18 13:23:54 +0530

    -

---
+++ first.js
@@ -0,0 +1,25 @@
+fullName = "Aryan Panchal";
+console.log(fullName);
+
+age = 14;
+console.log(age);
+
+price = 99.99;
+console.log(price);
+
+x = null;
+console.log(x); // we know the content but it's empty 
+
+y = undefined;
+console.log(undefined); // don't know what it is
+
+isFollow = false; // true or false 
+console.log(isFollow);
+
+age = "String";
+console.log(age);
+
+console.log("First JS Code");
+
+_$1a = "_ $ a-z A-Z is allowed in variable name";
+console.log(_$1a);
