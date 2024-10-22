641c4b1e99d0ac986ebdc3b9bd2321b68d9d228c
Aryan <Aryantech03@gmail.com>
2024-10-22 12:34:36 +0530

    -

---
+++ L2_Operators_&_Conditional_Statements/scripts.js
@@ -0,0 +1,27 @@
+console.log("hello world!", "JS");
+
+/* Arithmetic Operators  */
+let a = 9;
+let b = 7;
+let c = a + b;
+
+/* console.log("a : ", a);
+console.log("b : ", b); */
+
+console.log("a = ", a, "b = ", b);
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+
+console.log("a % b = ", a % b);
+console.log("a ** b = ", a ** b);
+
+/* Unary Operators - Operators only require a single variable */
+a++;
+console.log("a++ = ", a);
+
+b--;
+console.log("b-- = ", b);
+
