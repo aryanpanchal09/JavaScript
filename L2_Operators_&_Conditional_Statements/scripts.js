dc1e38ee60a1a0b7ede7881ce46f4a4dd61843b9
Aryan <Aryantech03@gmail.com>
2024-10-23 12:10:33 +0530

    -

--- L2_Operators_&_Conditional_Statements/scripts.js
+++ L2_Operators_&_Conditional_Statements/scripts.js
@@ -19,9 +19,52 @@ console.log("a % b = ", a % b);
 console.log("a ** b = ", a ** b);
 
 /* Unary Operators - Operators only require a single variable */
+/* Post Increment */
 a++;
 console.log("a++ = ", a);
 
 b--;
 console.log("b-- = ", b);
+
+/* Pre-Increment */
+++a;
+console.log("++a = ", a);
+
+--b;
+console.log("--b = ", b);
+
+/* Assignment Operator */
+
+let x = 5;
+let y = 2;
+
+console.log("x =",x ,"y =", y)
+
+x += 4; /* x = x + 4 */
+console.log("x = ", x); 
+
+x -= 4; /* x = x - 4 */
+console.log("x = ", x); 
+
+x *= 4; /* x = x * 4 */
+console.log("x = ", x); 
+
+x /= 4; /* x = x / 4 */
+console.log("x = ", x); 
+
+x **= 4; /* x = x ** 4 */
+console.log("x = ", x); 
+
+x %= 4; /* x = x % 4 */
+console.log("x = ", x); 
+
+/* Comparision Operator */
+
+let co1 = 5;
+let co2 = 2;
+
+console.log("5 == 2", co1 == co2);
+console.log("5 == 5", co1 == co2);
+
+console.log("5 ! = 2", co1 != co2);
 
