e1d175726eb67033cc1662fe22b9b5b5e6487381
Aryan <Aryantech03@gmail.com>
2024-12-07 13:12:35 +0530

    -

--- L4_Arrays/script.js
+++ L4_Arrays/script.js
@@ -37,4 +37,27 @@
     sum = sum/marks.length;
     console.log(sum);
 }
-
+/* Practise Question  
+For a given array with prices of 5 items -> [250, 645, 300, 900, 50]
+All items have an offer of 10% OFF on them. 
+Change the array to store final price after applying offer*/
+{
+    let items = [250, 645, 300, 900, 50];
+    let i = 0;
+    for(let val of items){
+        console.log(`value at index ${i} = ${val}`);
+        let offer = val / 10;
+        items[i] = items[i] - offer;
+        console.log(`value after offer = ${items[i]}`);
+        i++;
+    }
+}
+/* Done the same above but this time with the help of Array */
+{
+    let items = [250, 645, 300, 900, 50];
+    for(let i = 0; i < items.length; i++){
+        let offer = items[i] / 10;
+        items[i] -= offer;
+    }
+    console.log(items);
+}
