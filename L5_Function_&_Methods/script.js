b1d5a2e10c0bf6a09344716da0f3166817834ed5
Aryan <Aryantech03@gmail.com>
2025-01-29 13:57:38 +0530

    -

--- L5_Function_&_Methods/script.js
+++ L5_Function_&_Methods/script.js
@@ -1,64 +1,66 @@
 {
-    function myFunction() {
-        console.log("Welcome to JS");
-        console.log("We are practising JS");
-    }
+  function myFunction() {
+    console.log("Welcome to JS");
+    console.log("We are practising JS");
+  }
 
-    myFunction();
+  myFunction();
 }
 /* Parameter/argument -> input */
 {
-    function myFunction(msg, n) {
-        console.log(msg * n);
-    }
+  function myFunction(msg, n) {
+    console.log(msg * n);
+  }
 
-    myFunction("Hello World", 100);
+  myFunction("Hello World", 100);
 }
 /* Nan - Not a Number - An Error will be there */
 
 /* Function to calculate 2 number */
 {
-    function sum(x, y) {
-        console.log(x + y);
-    }
-    /* In console write sum(3,4) you want to add to get the desired result */
+  function sum(x, y) {
+    console.log(x + y);
+  }
+  /* In console write sum(3,4) you want to add to get the desired result */
 }
 
 {
-    function sum(x, y) {
-        s = x + y;
-        console.log("Before Return");
-        return s;
-        console.log("After Return"); /* This line will never run as it's written after return statement */
-    }
+  function sum(x, y) {
+    s = x + y;
+    console.log("Before Return");
+    return s;
+    console.log(
+      "After Return"
+    ); /* This line will never run as it's written after return statement */
+  }
 
-    let val = sum(3, 4);
-    console.log(val);
+  let val = sum(3, 4);
+  console.log(val);
 }
 
 {
-    for (let i = 0; i <= 5; i++) {
-        console.log(i, "hii");
-    }
+  for (let i = 0; i <= 5; i++) {
+    console.log(i, "hii");
+  }
 }
 
 /* Arrow Function  */
 {
-    function sum(a, b) {
-        return a + b;
-    }
+  function sum(a, b) {
+    return a + b;
+  }
 
-    const arrowSum = (a, b) => {
-        console.log(a + b)
-    }
+  const arrowSum = (a, b) => {
+    console.log(a + b);
+  };
 
-    function multi(a, b) {
-        return a * b;
-    }
+  function multi(a, b) {
+    return a * b;
+  }
 
-    const arrowMulti = (a, b) => {
-        console.log(a * b)
-    }
+  const arrowMulti = (a, b) => {
+    console.log(a * b);
+  };
 }
 
 const printHello = () => console.log("hello");
@@ -67,35 +69,47 @@ const printHello = () => console.log("hello");
 /* Q.1 Create a function using the function keyword that takes a String as an argument & returns the number of vowels in the string */
 
 function countVowels(str) {
-    let count = 0;
-    //"Hello", count = 2
-    for (const char of str) {
-        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-            count++;
-        };
-    };
-    console.log("The number of vowels is : ",count);
-};
+  let count = 0;
+  //"Hello", count = 2
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  console.log("The number of vowels is : ", count);
+}
 
 /* Create an arrow function to perform the same task */
 
 const countVow = (str) => {
-    let count = 0;
-    //"Hello", count = 2
-    for (const char of str) {
-        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-            count++;
-        };
-    };
-    console.log("The number of vowels is : ",count);
+  let count = 0;
+  //"Hello", count = 2
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  console.log("The number of vowels is : ", count);
 };
 
 /* forEach LOOP IN ARRAYS */
 function abc() {
-    console.log("hello");
+  console.log("hello");
 }
 function myFunc(abc) {
-    return abc;
+  return abc;
 }
 
 /* forEach Working Mechanism */
@@ -122,7 +136,7 @@ function myFunc(abc) {
     console.log(val.toUpperCase(), idx, arr);
   });
 }
-  
+
 /* practise question */
 /* For a given array of numbers, print the square of each value using the ForEach loop  */
 
@@ -153,3 +167,66 @@ function myFunc(abc) {
     return val ** 2;
   });
 }
+
+/* Filter Method */
+{
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  let evenArray = arr.filter((val) => {
+    return val % 2 === 0;
+    /* return val % 2 !== 0; - odd number
+     return val > 3; */
+  });
+
+  console.log(evenArray);
+}
+
+/* Reduce Method 
+val can also be written as curr
+result = previous/starting value 
+val / curr = current/next value 
+first add sum save in res only that's how the loop continues
+*/
+{
+  let arr = [1, 2, 3, 4];
+
+  const output = arr.reduce((res, val) => {
+    return res + val;
+  });
+
+  console.log(output);
+}
+/* To return the highest or lowerst number from the array */
+{
+  let arr = [1, 2, 3, 4, 5, 6];
+
+  const output = arr.reduce((res, val) => {
+    return res > val ? val || res : val;
+  });
+
+  console.log(output);
+}
+
+/* Practise Questions */
+/* We are given array of marks of students. Filter out the marks of students that scored 90+ */
+{
+  let arr = [85, 90, 91, 92];
+
+  let newArr = arr.filter((val) => {
+    return val >= 90;
+  });
+
+  console.log(newArr);
+}
+
+/* Take a number n as input from user. Create an array of numbers from 1 to n. Use the reduce method to calculate sum of all the numbers in the array. Use the reduce method to calculate product of all numbers in the array */
+let num = prompt("Enter a value between 1 to 10 : ");
+console.log(num);
+
+let arr = [];
+
+for(i=1; i<=num; i++){
+  arr[i-1] = i;
+}
+
+console.log(arr);
