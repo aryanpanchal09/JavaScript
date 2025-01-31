ebce2e0df6573f6c8e49f57064efce8b3f3671fd
Aryan <Aryantech03@gmail.com>
2025-01-31 08:53:14 +0530

    -

--- Dom_&_Events/script.js
+++ Dom_&_Events/script.js
@@ -1,1 +1,8 @@
 console.log("Document Object Model");
+console.dir(window);
+console.dir(window.document);
+console.dir(document.body); /* access html code through JS */
+console.log(document.body);
+console.log(document.head); /* available in child nodes */
+console.dir(document.body.childNodes[1]);
+document.body.childNodes[1].innerText="abcd";
