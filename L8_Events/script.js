70d0e70f207fa6cc6256733c9cbcfcaca7e6bef7
Aryan <Aryantech03@gmail.com>
2025-02-11 08:59:39 +0530

    -

--- L8_Events/script.js
+++ L8_Events/script.js
@@ -1,4 +1,42 @@
 let btn1 = document.querySelector("#btn1");
-btn1.ondblclick = () => {
+/* {
+  btn1.onclick = (evt) => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
     console.log("btn1 was clicked");
+    let a = 25;
+    a++;
+    console.log(a);
+  };
+} */
+{
+  let div = document.querySelector("div");
+  div.onmouseover = (evt) => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log("You are inside div");
+  };
 }
+btn1.addEventListener("click", (evt) => {
+    console.log("button1 was clicked - Handler1");
+});
+btn1.addEventListener("click", (evt) => {
+    console.log("button1 was clicked - Handler2");
+});
+
+const handler3 = () => {
+    console.log("button1 was clicked - Handler3");
+};
+
+btn1.addEventListener("click", handler3);
+
+btn1.addEventListener("click", (evt) => {
+    console.log("button1 was clicked - Handler4");
+});
+btn1.addEventListener("dblclick", () => {
+    console.log("button1 was clicked 2 times");
+});
+
+btn1.removeEventListener("click", handler3);
