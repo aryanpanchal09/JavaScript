b82c42501166968c6761a6f8509d72ac64f6d539
Aryan <Aryantech03@gmail.com>
2025-02-20 08:43:26 +0530

    -

--- L11_Classes_&_Objects/script.js
+++ L11_Classes_&_Objects/script.js
@@ -188,3 +188,52 @@ class Engineer extends Person {
 }
 
 let engObj = new Engineer("aryan");
+
+/* 
+PRACTISE QUESTIONS 
+------------------
+You are creating a website for your colledge. Create a class User with 2 properties, name & email. It also has a method called viewData() that allows user to view website data  */
+
+let DATA = "secret information";
+
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  viewData() {
+    console.log("data = ", DATA);
+  }
+}
+
+class Admin extends User {
+  constructor(name, email) {
+    super(name, email);
+  }
+  editData() {
+    DATA = "some new value";
+  }
+}
+
+let student1 = new User("aryan", "abc@gmail.com");
+let student2 = new User("umang", "umang@gmail.com");
+
+let teacher1 = new User("Dean", "dean@gmail.com");
+
+let admin1 = new Admin("admin", "admin@gmail.com");
+
+/* Create a new class called Admin which inherits from User. Add a new method called editData to Admin that allows it to edit website data */
+
+/* ERROR HANDLING */
+let a = 5;
+let b = 10;
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a + b =", a + b);
+try {
+  console.log("a + b = ", a + c);
+} catch (err) {
+  console.log(err);
+}
+console.log("Code executing after try and catch block");
