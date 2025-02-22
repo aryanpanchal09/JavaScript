5d61d585fc1e4a2bbcb5f50117cbcffce05f1c89
Aryan <Aryantech03@gmail.com>
2025-02-22 08:51:36 +0530

    -

--- L12_Callbacks_Promises_Async_Await/script.js
+++ L12_Callbacks_Promises_Async_Await/script.js
@@ -19,23 +19,71 @@ console.log("four");
 console.log("five");
 
 /* CALLBACK FUNCTION */
-
-function sum(a,b) {
+{
+  function sum(a, b) {
     console.log(a + b);
-}
-/* sumCallback can also be written as sum - it's written just for better understanding of callback function*/
-function calculator(a, b, sumCallback) {
+  }
+  /* sumCallback can also be written as sum - it's written just for better understanding of callback function*/
+  function calculator(a, b, sumCallback) {
     sumCallback(a, b);
-}
+  }
 
-calculator(1, 2, sum);
+  calculator(1, 2, sum);
 
-/* calcularot(1, 2, (a, b) => {
+  /* calcularot(1, 2, (a, b) => {
     console.log(a + b);
 }) */
 
-const hello = () => {
+  const hello = () => {
     console.log("hello after 3sec");
-};
+  };
 
-setTimeout(hello, 3000);
+  setTimeout(hello, 3000);
+}
+
+/* Nested if else */
+{
+  let age = 19;
+  if (age >= 18) {
+    if (age >= 60) {
+      console.log("senior");
+    } else {
+      console.log("Your Age is in Middle Section");
+    }
+  } else {
+    console.log("child");
+  }
+}
+
+// Nested for loop 
+
+{
+  for(let i=0; i<5; i++){
+    let str = "";
+    for (let j=0; j<5; j++) {
+      str = str + j;
+    }
+    console.log(i, str);
+  }
+}
+
+/* Database recreate */
+function getData(dataId, getNextData) {
+  setTimeout(() => {
+    console.log("data", dataId);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 1000);
+}
+
+/* Callback hell example */
+getData(1, () => {
+  console.log("getting Data 2...");
+  getData(2, () => {
+    console.log("getting Data 3...");
+    getData(3);
+  });
+});
+
+
