289ab3aad2e5937423158049db99a03dddb1bc27
Aryan <Aryantech03@gmail.com>
2025-03-01 09:37:13 +0530

    -

--- L12_Callbacks_Promises_Async_Await/app1.js
+++ L12_Callbacks_Promises_Async_Await/app1.js
@@ -1,3 +1,43 @@
+async function hello() {
+  console.log("hello");
+}
+
+function api() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("weather data");
+      resolve(200);
+    }, 2000);
+  });
+}
+
+async function getWeatherData() {
+  await api();
+  await api();
+}
+
+function getData(dataId) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data", dataId);
+      resolve("success");
+    }, 2000);
+  });
+}
+
+//Async-await
+async function getAllData() {
+  console.log("Data 01");
+  await getData(1);
+  console.log("Data 02");
+  await getData(2);
+  console.log("Data 03");
+  await getData(3);
+  console.log("Data 04");
+  await getData(4);
+}
+
+/*
 function getData(dataId, getNextData) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -6,13 +46,13 @@ function getData(dataId, getNextData) {
       resolve("success");
       /*       if (getNextData) {
         getNextData();
-      } not required as we are using promise.then */
+      } not required as we are using promise.then 
     }, 3000);
   });
 }
-
+*/
 // Promise Chain
-
+/* 
 getData(1)
   .then((res) => {
     return getData(2);
@@ -23,6 +63,7 @@ getData(1)
   .then((res) => {
     console.log("success");
   });
+*/
 
 /* let p1 = getData(1);
 p1.then((res) => {
