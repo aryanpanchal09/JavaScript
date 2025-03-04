f931097135e456ad305f07175649fcffd076201c
Aryan <Aryantech03@gmail.com>
2025-03-04 09:10:51 +0530

    -

--- L13_Api_Project/script.js
+++ L13_Api_Project/script.js
@@ -1,11 +1,26 @@
 const URL = "https://catfact.ninja/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
 
 const getFacts = async () => {
-    console.log("getting data ...");
-    let response = await fetch(URL);
-    console.log(response);
-    let data = await response.json();
-    console.log(data.data[2].fact);
+  console.log("getting data ...");
+  let response = await fetch(URL);
+  console.log(response);
+  let data = await response.json();
+  console.log(data.data[2].fact);
+  factPara.innerText = data.data[0].fact;
 };
 
-getFacts();
+/* Promise Chain 
+function getFacts() {
+  fetch(URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      factPara.innerText = data[2].text;
+    });
+} */
+
+btn.addEventListener("click", getFacts);
