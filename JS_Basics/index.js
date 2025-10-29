46e592a2f484b941383449d15cbd7f9cccfbaf11
unknown <aryantech03@gmail.com>
2025-10-29 23:44:27 +0530

    console table

---
+++ JS_Basics/index.js
@@ -0,0 +1,11 @@
+console.log('hello');
+
+const accId = 1234;
+console.log(accId);
+
+let email = "aryantech03@gmail.com";
+var pass = "1234";
+
+// accId = "asdf";
+
+console.table([accId, email, pass, accId])
