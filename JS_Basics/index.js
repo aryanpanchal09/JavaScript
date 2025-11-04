77b55be57676e41ea5e2d115e5f4192a5bd4adb0
unknown <aryantech03@gmail.com>
2025-11-04 23:58:14 +0530

    -

--- JS_Basics/index.js
+++ JS_Basics/index.js
@@ -173,3 +173,8 @@ console.log(Math.abs(-4));
 console.log(Math.round(4.8));
 console.log(Math.ceil(4.5));
 console.log(Math.floor(4.5));
+
+const max = 30;
+const min = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
