22f4877ac3255452477e6906b9c0e02e99b50cc8
unknown <aryantech03@gmail.com>
2025-11-24 23:52:26 +0530

    function & return

--- JS_Basics/index.js
+++ JS_Basics/index.js
@@ -383,3 +383,11 @@ function five(){
 }
 
 five();
+
+function addtownumber(number1, number2){
+    // let result = number1 + number2;
+    // return result;
+    return number1 + number2;
+}
+let sum = addtownumber(1,2);
+console.log(sum)
